0e1f622f4d0a01fc0607e04217a431ea0b13550c
Parksolmi <69065439+Parksolmi@users.noreply.github.com>
2021-09-16 01:48:19 +0900

    Polynomial.h의 함수 수정
    mult함수 논리적 오류 수정.
    add함수 논리적 오류 수정.

--- Week1_Ch1,Ch2/Week1_Ch1,Ch2/Polynomial.cpp
+++ Week1_Ch1,Ch2/Week1_Ch1,Ch2/Polynomial.cpp
@@ -1,33 +1,35 @@
 #include "Polynomial.h"
 
-void main()
+int main()
 {
 	Polynomial pnm1, pnm2, pnm3;
 
 	cout << "다항식1 작성 => " << endl; pnm1.read();
 	cout << "다항식2 작성 => " << endl; pnm2.read();
-	cout << "다항식1 : "; pnm1.display();
+	cout << "다항식1 : "; pnm1.display(); cout << endl;
 	cout << "다항식2 : "; pnm2.display();
 
-	cout << endl;
+	cout << endl << endl;
 	pnm3.add(pnm1, pnm2);
 	cout << "----다항식 1과 2의 합----" << endl;
 	pnm3.display();
 	
-	cout << endl;
+	cout << endl <<endl;
 	pnm3.mult(pnm1, pnm2);
 	cout << "----다항식 1과 2의 곱----" << endl;
 	pnm3.display();
 
-	cout << endl;
+	cout << endl << endl;
 	cout << "----부호바꾸기----" << endl;
 	pnm1.negate();
 	pnm2.negate();
-	cout << "다항식1 : "; pnm1.display();
+	cout << "다항식1 : "; pnm1.display(); cout << endl;
 	cout << "다항식2 : "; pnm2.display();
 
-	cout << endl;
+	cout << endl << endl;
 	cout << "----최고차항이 0인지 검사----" << endl;
-	cout << "다항식1 : "; pnm1.isZero();
-	cout << "다항식2 : "; pnm2.isZero();
+	bool result = pnm1.isZero();
+	cout << "다항식1 : " << boolalpha << result << endl;
+	result = pnm2.isZero();
+	cout << "다항식2 : " << boolalpha << result << endl;
 }
--- Week1_Ch1,Ch2/Week1_Ch1,Ch2/Polynomial.h
+++ Week1_Ch1,Ch2/Week1_Ch1,Ch2/Polynomial.h
@@ -7,15 +7,17 @@ const int MAX_DEGREE = 80; //다항식의 처리 가능한 최대 차수+1
 class Polynomial
 {
 private:
+
 	int degree; //최고차항계수
-	float coef[]; //다항식을 저장할 배열
+	float coef[MAX_DEGREE]; //다항식을 저장할 배열
 				  //배열의 인덱스가 다항식의 차수
 
 public:
 	Polynomial() //생성자
 	{
 		degree = 0;
-	};
+		coef[0] = { 0 };
+	}
 	void read() //다항식을 입력하는 함수
 	{
 		cout << "다항식의 최고 차수 : ";
@@ -26,24 +28,24 @@ public:
 			cin >> coef[i];
 		}
 		cout << endl;
-	};
+	}
 	bool isZero() //최고차항이 0인지 확인하는 함수
 	{
-		return degree == 0;
-	};
+		return (degree == 0) ? true : false;
+	}
 	void negate() //모든 계수의 부호를 바꾸는 함수
 	{
 		for (int i = 0; i <= degree; i++)
 		{
 			coef[i] *= (-1);
 		}
-	};
+	}
 	void add(Polynomial p1, Polynomial p2) //다항식을 더하는 함수
 	{
 		if (p1.degree > p2.degree)
 		{
 			*this = p1;
-			for (int i = 0; i < p2.degree; i++)
+			for (int i = 0; i <= p2.degree; i++)
 			{
 				coef[i] += p2.coef[i];
 			}
@@ -51,32 +53,46 @@ public:
 		else
 		{
 			*this = p2;
-			for (int i = 0; i < degree; i++)
+			for (int i = 0; i <= p1.degree; i++)
 			{
 				coef[i] += p1.coef[i];
 			}
 		}
-	};
+	}
 	void mult(Polynomial p1, Polynomial p2) //다항식을 곱하는 함수
 	{
-		Polynomial temp1, temp2;
-		Polynomial temp = temp1;
-		for (int i = 0; i < p1.degree; i++)
+		*this = p1;
+		int temp = degree;
+		degree += p2.degree; //곱한 결과로서의 최고차항의 차수 저장
+
+		int* temporary = new int[degree + 1]; //임시 배열 만들기 - 동적할당 : 곱셈의 결과를 임시로 저장할 배열
+		for (int i = 0; i <= degree; i++) //배열 초기화
 		{
-			for (int j = 0; j < p2.degree; j++)
-			{
-				temp.coef[i + j] = p1.coef[i] * p2.coef[j];
-			}
-			temp = temp2;
+			temporary[i] = 0;
 		}
-		add(temp1, temp2);
-	};
+		
+		for (int i = 0; i <= temp; i++)
+		{
+			for (int j = 0; j <= p2.degree; j++)
+			{
+				temporary[i + j] += coef[i] * p2.coef[j]; //계수의 곱
+			}
+		}
+
+		for (int i = 0; i <= degree; i++)
+		{
+			coef[i] = temporary[i]; //임시 배열에서 객체의 배열로 데이터 옮기기
+		}
+
+		delete[] temporary; // 메모리 반환
+	}
 	void display() //다항식을 화면에 출력하는 함수
 	{
-		for (int i = degree; i >= 0; i--) //최고차항부터 출력
+		for (int i = degree; i > 0; i--) //최고차항부터 출력
 		{
 			cout << coef[i] << "x^" << i << " + ";
 		}
-	};
+		cout << coef[0];
+	}
 
 };
